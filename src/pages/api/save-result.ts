e12f7cc9bf6f0cdd7bc5999659f4c1aa96c05427
Grant McNatt <60593837+ZoOtMcNoOt@users.noreply.github.com>
2024-11-14 01:44:13 -0600

    word

--- src/pages/api/save-result.ts
+++ src/pages/api/save-result.ts
@@ -1,5 +1,3 @@
-// src/pages/api/save-result.ts
-
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { supabaseServer } from '../../lib/supabaseClient';
 import { v4 as uuidv4 } from 'uuid';
