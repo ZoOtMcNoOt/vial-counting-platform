982a9a5f612c70e37c1413777a022fadb60df3f4
Grant McNatt <60593837+ZoOtMcNoOt@users.noreply.github.com>
2024-11-14 00:07:40 -0600

    speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '../context/ThemeContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ThemeProvider>
+      <SpeedInsights/>
+    </>
   );
 }
 
