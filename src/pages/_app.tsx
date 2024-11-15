11a7cffcc18b05ba21e75c14781e0540405f356f
Grant McNatt <60593837+ZoOtMcNoOt@users.noreply.github.com>
2024-11-14 19:57:49 -0600

    Add favicon link to Head component in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ThemeProvider } from '../context/ThemeContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -9,6 +10,9 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/images/favicon/favicon.webp" type="image/webp" />
+      </Head>
       <ThemeProvider>
         <QueryClientProvider client={queryClient}>
           <Component {...pageProps} />
